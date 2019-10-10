ba65a1642e920ba3da57fef4153b9870ac4a4fe5
gennadylaptev <gennadylaptev@gmail.com>
2019-10-10 17:50:27 +0300

    F

---
+++ 00/sum_by_rows.cpp
@@ -0,0 +1,50 @@
+#include <chrono>
+#include <iostream>
+
+class Timer
+{
+    using clock_t = std::chrono::high_resolution_clock;
+    using microseconds = std::chrono::microseconds;
+public:
+    Timer()
+        : start_(clock_t::now())
+    {
+    }
+
+    ~Timer()
+    {
+        const auto finish = clock_t::now();
+        const auto us = 
+            std::chrono::duration_cast<microseconds>
+                (finish - start_).count();
+        std::cout << us << " us" << std::endl;
+    }
+
+private:
+    const clock_t::time_point start_;
+};
+
+int main() {
+
+    int size = 10000;
+     
+    int ** a = new int*[size];
+    for (int i = 0; i < size; i++)
+        a[i] = new int[size];
+        
+    for (int j = 0; j < size; j++) {
+        for (int k = 0; k < size; k++)
+            a[j][k] = rand() % 10000;
+    }
+    
+	// We use volatile here to prevent the compiler from "remembering"
+	// the sum value    
+    int sum = 0;
+	Timer timer;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++)
+            sum += a[i][j];
+    } 
+    
+    return 0;
+}
